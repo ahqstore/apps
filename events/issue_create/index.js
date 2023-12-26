2177f6b23db423161dccc15a804a92334bfcb510
AHQ Miness <ahqsecret@gmail.com>
2023-12-26 18:57:17 +0530

    Stuff

--- events/issue_create/index.js
+++ events/issue_create/index.js
@@ -1,12 +1,55 @@
+const { join } = require("node:path");
+
+const prefix = "/ahqstorebot";
+
 /**
- *
+ * Handle
  * @param {import("@octokit/rest").Octokit} github
- * @param {{ payload: { comment: {body: string }, issue: { id: number, labels: string[] } }}} ctx
+ * @param {{ payload: { comment: {body: string }, issue: { id: number, labels: {name: string}[] } }}} ctx
  */
 module.exports = (github, ctx) => {
-  console.log(
-    ctx.payload.comment.body,
-    ctx.payload.issue.id,
-    ctx.payload.issue.labels
-  );
+  const comment = ctx.payload.comment.body;
+  const issueId = ctx.payload.issue.id;
+  const labels = ctx.payload.issue.labels;
+  const manifestPath = join(__dirname, "..", "..", "manifests");
+
+  const base = {
+    issue_number: issueId,
+    owner: "ahqstore",
+    repo: "apps",
+  };
+
+  const hasAppUpdateLabel = labels.includes(({ name }) => name == "app update");
+
+  function createComment(body) {
+    github.rest.issues.createComment({
+      ...base,
+      body,
+    });
+  }
+
+  if (comment == `${prefix} -set-app-update`) {
+    if (!hasAppUpdateLabel) {
+      github.rest.issues.addLabels({
+        ...base,
+        labels: ["app update"],
+      });
+      createComment("✅ Labels Assigned");
+    } else {
+      createComment("❌ Labels Already Assigned");
+    }
+  } else if (comment.startsWith(`${prefix} /app-newConf-set`)) {
+    if (hasAppUpdateLabel) {
+      createComment(
+        [
+          `# 🚧 Under Development`,
+          `IssueNumber: \`${issueId}\``,
+          `Labels: \`${labels.map(({ name }) => name).join(", ")}\``,
+          `ManifestPath: \`${manifestPath}\``,
+        ].join("\n")
+      );
+    } else {
+      createComment("❌ Labels Not Assigned");
+    }
+  }
 };
