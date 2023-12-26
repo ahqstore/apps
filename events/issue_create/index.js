fab779de110e2565ee68bddd5f16b8e24e4cf8b8
AHQ Miness <ahqsecret@gmail.com>
2023-12-26 18:04:08 +0530

    Stuff

--- events/issue_create/index.js
+++ events/issue_create/index.js
@@ -1,7 +1,8 @@
 /**
  *
  * @param {import("@octokit/rest").Octokit} github
+ * @param {{ payload: { body: string }, issue: { id: number } }} ctx
  */
 module.exports = (github, ctx) => {
-  console.log(ctx);
+  console.log(ctx.payload.body, ctx.issue.id);
 };
