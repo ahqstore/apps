85fba15bb7e399f88aba02524453bab9557aee8b
AHQ Miness <ahqsecret@gmail.com>
2024-02-28 15:08:31 +0530

    Run every day

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,64 @@
+use ahqstore_types::AHQStoreApplication;
+use inquire::{Confirm, Text};
+use serde_json::from_str;
+use std::fs;
+
+#[cfg(feature = "parse")]
+mod parser;
+mod types;
+
+#[cfg(feature = "parse")]
+use parser::*;
+use types::*;
+
+macro_rules! ask {
+    ($val:ident: bool, $($x:tt)*) => {
+        let $val = Confirm::new($($x)*).with_default(true).prompt().unwrap();
+        println!("");
+    };
+
+    ($val:ident: String, $($x:tt)*) => {
+        let $val = Text::new($($x)*).prompt().unwrap();
+        println!("");
+    }
+}
+
+macro_rules! pan {
+  ($($x:tt)*) => {
+    println!($($x)*);
+    std::process::exit(1);
+  };
+}
+
 fn main() {
-    println!("Hello, world!");
+  #[cfg(feature = "parse")]
+  parser();
+
+  #[cfg(feature = "gen")]
+  run();
+}
+
+fn run() {
+  let _ = fs::create_dir_all("./request");
+  let _ = fs::write("./request/app.json", "{}");
+
+  ask!(val: bool, "Fill ./request/app.json with the contents of your app manifest");
+
+  if !val {
+    pan!("You write write \"Y\"");
+  }
+
+  let store: AHQStoreApplication =
+    from_str(&fs::read_to_string("./request/app.json").unwrap()).unwrap();
+
+  let bytes = Data(store).to_bytes();
+
+  fs::write("./bytes.txt", &bytes).unwrap();
+  fs::remove_dir_all("./request").unwrap();
+}
+
+fn gen_appid() -> String {
+  let mut string = String::new();
+
+  string
 }
---
+++ src/parser.rs
@@ -0,0 +1,133 @@
+use ahqstore_types::AHQStoreApplication;
+use serde_json::{from_str, to_string_pretty};
+use std::{
+  fs::{self, File},
+  io::Write,
+};
+
+struct Map {
+  entries: usize,
+  files: usize,
+  c_file: File,
+  search: File,
+}
+
+impl Map {
+  fn new() -> Self {
+    let _ = fs::create_dir_all("./db/map");
+    let _ = fs::create_dir_all("./db/search");
+    let _ = fs::create_dir_all("./db/apps");
+
+    let mut file = File::create("./db/map/1.json").unwrap();
+    let _ = file.write(b"{");
+
+    let mut search = File::create("./db/search/1.json").unwrap();
+    let _ = search.write(b"[");
+
+    Self {
+      entries: 0,
+      files: 1,
+      c_file: file,
+      search,
+    }
+  }
+
+  fn close_file(&mut self) {
+    let _ = self.search.write_all(b"]");
+    let _ = self.search.flush();
+    let _ = self.c_file.write_all(b"}");
+    let _ = self.c_file.flush();
+  }
+
+  fn new_file(&mut self) {
+    self.files += 1;
+    self.entries = 0;
+    self.close_file();
+
+    let mut map = File::create("./db/map/1.json").unwrap();
+    let _ = map.write(b"{");
+
+    let mut search = File::create("./db/map/1.json").unwrap();
+    let _ = search.write(b"[");
+
+    self.c_file = map;
+    self.search = search;
+  }
+
+  fn add(&mut self, app: AHQStoreApplication) {
+    if self.entries >= 100_000 {
+      self.new_file();
+    }
+    if self.entries > 0 {
+      let _ = self.c_file.write(b",");
+      let _ = self.search.write(b",");
+    }
+    let _ = self
+      .c_file
+      .write(format!("\"{}\":\"{}\"", app.appDisplayName, app.appId).as_bytes());
+    let _ = self.search.write(
+      format!(
+        "{{\"name:\": {:?}, \"title\": {:?}, \"id\": {:?}}}",
+        app.appDisplayName, app.appShortcutName, app.appId
+      )
+      .as_bytes(),
+    );
+
+    let _ = fs::write(
+      format!("./db/apps/{}.json", app.appId),
+      to_string_pretty(&app).unwrap(),
+    );
+  }
+
+  fn finish(mut self) {
+    self.close_file();
+
+    let _ = fs::write("./db/total", self.files.to_string());
+  }
+}
+
+pub fn parser() {
+  println!("⏲️ Please wait...");
+  let _ = fs::remove_dir_all("./db");
+  let _ = fs::create_dir_all("./db");
+
+  let _ = fs::copy("./manifests/home.json", "./db/home.json");
+
+  let mut map = Map::new();
+
+  for letter in fs::read_dir("./manifests").unwrap() {
+    let ltr = letter.unwrap();
+
+    if ltr.file_name().to_string_lossy() != "home.json" {
+      for user in fs::read_dir(ltr.path()).unwrap() {
+        let usr = user.unwrap();
+
+        let name = usr.file_name();
+        let name = name.to_string_lossy();
+        if &name != "ignore" {
+          for repo in fs::read_dir(usr.path()).unwrap() {
+            let repo = repo.unwrap();
+
+            let id = repo.file_name();
+            let id = id.to_string_lossy();
+
+            if &id != "user" {
+              let data = fs::read_to_string(repo.path()).unwrap();
+              let data: AHQStoreApplication = from_str(&data).unwrap();
+
+              println!(
+                "🔎 {}->{}->{}",
+                name.split_at(1).0,
+                name,
+                &data.appDisplayName
+              );
+              map.add(data);
+            }
+          }
+        }
+      }
+    }
+  }
+  map.finish();
+  println!("✅ Done!");
+}
