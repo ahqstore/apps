4ae946912113a858ac0d0fced5f157aa2cbc81b7
AHQ Miness <ahqsecret@gmail.com>
2024-02-29 20:38:02 +0530

    new way of bytes.txt

--- events/issue_comment/index.js
+++ events/issue_comment/index.js
@@ -1,13 +1,54 @@
+//@ts-check
+const { exec } = require("child_process");
+const { writeFileSync } = require("fs");
+const { join } = require("path");
+
 /**
  * Handle
  * @param {import("@octokit/rest").Octokit} github
- * @param {{ payload: { comment: {body: string }, issue: { number: number, id: number, labels: {name: string}[] } }}} ctx
+ * @param {{ payload: { comment: { id: number, body: string, user: { login: string } }, issue: { number: number, id: number, labels: {name: string}[] } }}} ctx
  */
 module.exports = async (github, ctx) => {
   console.log(JSON.stringify(ctx, null, 4));
   const issue = ctx.payload.issue.id;
 
-  if (issue == 3) {
-    const data = await github.issues.createComment({});
+  const author_username = ctx.payload.comment.user.login;
+
+  const body = ctx.payload.comment.body;
+
+  const [slash, cmd, link] = body.split(" ");
+
+  const owner = "ahqstore";
+  const repo = "apps";
+
+  if (issue == 3 && slash == "/store") {
+    if (cmd == "set") {
+      const req = await fetch(link).then((req) => req.text());
+      writeFileSync("./bytes.txt", `${owner}&${req}`);
+
+      const workspace = join(__dirname, "../../");
+      exec(
+        "cargo run --no-default-features --features load_bytes",
+        {
+          cwd: workspace,
+        },
+        async (err, out, stderr) => {
+          const body = `@${author_username}
+# Output
+${out}
+# Error
+${err}
+${stderr}`;
+
+          await github.issues.createComment({
+            body,
+            owner,
+            repo,
+            issue_number: issue,
+          });
+        }
+      );
+    } else if (cmd == "remove") {
+    }
   }
 };
--- src/load.rs
+++ src/load.rs
@@ -6,12 +6,21 @@ use crate::Data;
 
 pub fn run() {
   let val = fs::read_to_string("./bytes.txt").unwrap();
+
+  let [gh_author, val] = val.split("&").collect::<Vec<_>>()[..] else {
+    panic!("Something went wrong");
+  };
+
   let val = Data::from_bytes(&val);
 
   let app = val.0;
 
   let app_txt = to_string_pretty(&app).unwrap();
   let author = &app.repo.author;
+
+  if &gh_author != &author {
+    panic!("Author Mismatch");
+  }
 
   let ltr = author.split_at(1).0;
   let _ = fs::create_dir_all(format!("./manifests/{}/{}", &ltr, &author));
